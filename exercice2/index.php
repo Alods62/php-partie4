7298d7b21190af414a2705cadc23cb7e07f754b7
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 13:43:13 +0100

    Add files via upload

---
+++ exercice2/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 4</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 2</h1>
+    <?php
+    
+    function echoString($string){
+        echo $string;
+        
+        return;
+    }
+    
+    echoString("Hello World !");
+    
+    ?>  
+    
+    
+</body>
+</html>
