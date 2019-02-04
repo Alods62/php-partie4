8bf060ad3ec317798d26424a4156f72f62d01b88
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 13:43:43 +0100

    Add files via upload

---
+++ exercice3/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 4</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 3</h1>
+    <?php
+    
+    function concatenateStrings($string1,$string2){
+        echo $string1 . ' ' . $string2;
+    }
+    
+    concatenateStrings("Salut","tout le monde");
+    
+    ?>  
+    
+    
+</body>
+</html>
