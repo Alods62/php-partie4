223e8382e1682f0b47a1701da30355f8407585d5
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-04 13:42:44 +0100

    Add files via upload

---
+++ exercice1/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 4</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 1</h1>
+    <?php
+    
+    function returnTrue(){ // on déclare une fonction, function nomDeLaFonction(paramètres), on laisse les parenthèses même si il n'y a pas de paramètres. 
+        return true;
+    }
+    
+    echo returnTrue(); // on appelle la fonction
+    
+    ?>  
+    
+    
+</body>
+</html>
